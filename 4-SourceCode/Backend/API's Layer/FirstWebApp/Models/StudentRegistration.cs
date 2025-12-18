fe5fa085a9ef7bdc0f6d2724d0a1a405a2ecc8e9
Qotiph <khaledbyayadh@gmail.com>
2025-12-18 17:05:52 +0300

    test

---
+++ 4-SourceCode/Backend/API's Layer/FirstWebApp/Models/StudentRegistration.cs
@@ -0,0 +1,11 @@
+﻿namespace FirstWebApp.Models
+{
+    public class StudentRegistration
+    {
+        public int ID { get; set; }
+        public string FullName { get; set; }
+        public string Department { get; set; }
+        public string Phone { get; set; }
+
+    }
+}
